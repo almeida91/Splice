e0d8bcbb9ec7af758c02cdf8a40c65bbe83138c4
Igor Almeida <cmte.igor.almeida@gmail.com>
2012-10-04 01:01:59 -0300

    working on a skeleton for ClonAlg

--- src/splice/ais/Population.java
+++ src/splice/ais/Population.java
@@ -6,20 +6,21 @@ import splice.RandomComponent;
 import java.util.Random;
 
 /**
- *
  * @author igor
  */
-public abstract class Antigen implements RandomComponent, InitializeComponent {
+public class Population implements RandomComponent, InitializeComponent {
     private Random random;
 
-    @Override
     public Random getRandom() {
         return random;
     }
 
-    @Override
     public void setRandom(Random random) {
         this.random = random;
     }
 
+    @Override
+    public void initialize() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
 }
