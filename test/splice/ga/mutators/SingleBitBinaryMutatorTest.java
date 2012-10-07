35c3a5ad0430c5694010bf90e5db22736b4acedc
Igor Almeida <cmte.igor.almeida@gmail.com>
2012-10-06 21:40:55 -0300

    tests...

--- test/splice/ga/mutators/SingleBitBinaryMutatorTest.java
+++ test/splice/ga/mutators/SingleBitBinaryMutatorTest.java
@@ -18,6 +18,7 @@ import java.util.Random;
 import org.junit.Before;
 import org.junit.Test;
 
+import splice.RandomComponentTest;
 import splice.ga.genes.BinaryGene;
 
 public class SingleBitBinaryMutatorTest {
@@ -43,4 +44,9 @@ public class SingleBitBinaryMutatorTest {
 		// verifies that the hamming distance is just one bit
 		assertEquals(1, gene.getValue().xor(originalValue).bitCount());
 	}
+
+    @Test
+    public void testRandom() {
+        RandomComponentTest.doTest(mutator);
+    }
 }
