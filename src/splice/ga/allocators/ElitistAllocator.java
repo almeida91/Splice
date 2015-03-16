ba1c6139629711e651ac0411634585e4ba2bfaae
Igor de Almeida <igor.almeida@tron.com.br>
2015-03-16 17:54:02 -0300

    elitistallocator was appending one chromosome more

--- src/splice/ga/allocators/ElitistAllocator.java
+++ src/splice/ga/allocators/ElitistAllocator.java
@@ -46,7 +46,7 @@ public class ElitistAllocator extends PopulationAllocator {
 		getPopulation().sort();
 		
 		if (getProblemType().isMaximization()) {
-			for (int i = 1; i <= n; i++) {
+			for (int i = 1; i < n; i++) {
 				append(getPopulation().get(getPopulation().getSize() - i));
 			}
 		}
